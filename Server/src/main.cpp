5ea08c2ece83c677a310a4f4c02bb7f6c9f4254c
Thisara Ranadeva <rta1sgp@bosch.com>
2021-01-16 17:28:02 +0800

    sekeloton added to test REST sdk

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -1,3 +1,32 @@
+#include<cpprest/http_listener.h>
+#include<iostream>
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+
+#define TRACE(msg)            std::wcout << msg
+
 int main () {
+
+    http_listener listner(L"http://localhost/restdemo");
+
+    listner.support(methods::GET,  [](http_request request) {
+        request.reply(200);
+    });
+
+	try
+	{
+		listner.open().then([&listner]() {TRACE(L"\nstarting to listen\n"); }).wait();
+		while (true)
+		{
+
+		}
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << "Error "<< ex.what() << std::endl;
+	}
+
     return 0;
 }
