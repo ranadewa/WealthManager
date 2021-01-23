930851ee59564d248e8f2865de01fbf44d3883cf
Ranadeva Thisara <ranadewa.thisara@gmail.com>
2021-01-23 18:01:59 +0800

    3rdParty build taken out of main build.

--- cmake/FindCppRest.cmake
+++ cmake/FindCppRest.cmake
@@ -2,15 +2,16 @@
 
 include(GNUInstallDirs) 
 
+message("Source Dir: " ${CMAKE_SOURCE_DIR})
 find_library(
   CppRest_LIBRARY 
-  NAMES libcpprest
-  HINTS ${PROJECT_BINARY_DIR}/3rdParty/cpprestsdk/Release/Binaries/
+  NAMES libcpprest.so
+  HINTS ${CMAKE_SOURCE_DIR}/3rdParty/build/cpprestsdk/Release/Binaries/
 )
  
-find_path(CppRest_ROOT_DIR include/cpprest/http_client.h 
+find_path(CppRest_INCLUDE_DIR include/cpprest/http_client.h 
   HINTS
-    ${CMAKE_SOURCE_DIR}/3rdParty/cpprestsdk/Release/Binaries
+    ${CMAKE_SOURCE_DIR}/3rdParty/cpprestsdk/Release
 )
 
 include(FindPackageHandleStandardArgs)
@@ -19,3 +20,11 @@ find_package_handle_standard_args(CppRest DEFAULT_MSG CppRest_INCLUDE_DIR CppRes
 mark_as_advanced(CppRest_INCLUDE_DIR CppRest_LIBRARY CppRest_ARTIFACT_RELEASE CppRest_ARTIFACT_DEBUG)
 
 
+if(CppRest_FOUND AND NOT TARGET CppRest::CppRest)
+  add_library(CppRest::CppRest SHARED IMPORTED)
+  set_target_properties(
+    CppRest::CppRest
+    PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${CppRest_INCLUDE_DIR}"
+      IMPORTED_LOCATION ${CppRest_LIBRARY})
+endif()
