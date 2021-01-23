ff8efbe94be888dc42dadab508ac7f0b32aa4619
Thisara Ranadeva <ranadewa.thisara@gmail.com>
2021-01-23 08:04:25 +0000

    include path updatd.

--- cmake/FindCppRest.cmake
+++ cmake/FindCppRest.cmake
@@ -25,9 +25,10 @@
 message("C2 :" ${CMAKE_SOURCE_DIR}) 
 find_path(CppRest_ROOT_DIR include/cpprest/http_client.h 
   HINTS
-    ${CMAKE_SOURCE_DIR}/CppRestSdk
+    ${CMAKE_SOURCE_DIR}/3rdParty/cpprestsdk/Release
 )
 
+message("C3 " ${CppRest_ROOT_DIR})
 if(CppRest_ROOT_DIR)
   message(STATUS "CppRest root directory ${CppRest_ROOT_DIR}")
   set(CppRest_INCLUDE_DIR ${CppRest_ROOT_DIR}/include)
@@ -46,31 +47,21 @@ if(CppRest_ROOT_DIR)
   set(CppRest_LIBRARY_DEBUG "${CppRest_DEBUG}")
   set(CppRest_LIBRARY_RELEASE "${CppRest_RELEASE}")
 
-  if(WIN32)
-    file(GLOB CppRest_ARTIFACT_RELEASE ${CppRest_ROOT_DIR}/bin/cpprest.dll)
-    file(GLOB CppRest_ARTIFACT_DEBUG ${CppRest_ROOT_DIR}/bin/cpprestd.dll)
-  elseif(UNIX)
-    file(GLOB CppRest_ARTIFACT_RELEASE ${CppRest_ROOT_DIR}/lib/libcpprest.so*)
-    file(GLOB CppRest_ARTIFACT_DEBUG ${CppRest_ROOT_DIR}/lib/libcpprestd.so*)
-  endif()
-  
+ file(GLOB CppRest_ARTIFACT_RELEASE ${CppRest_ROOT_DIR}/lib/libcpprest.so*)
+ file(GLOB CppRest_ARTIFACT_DEBUG ${CppRest_ROOT_DIR}/lib/libcpprestd.so*)
+
   # Define CppRest_LIBRARY
   include(SelectLibraryConfigurations)
   select_library_configurations(CppRest)
 
   mark_as_advanced(CppRest_LIBRARY_DEBUG, CppRest_LIBRARY_RELEASE)
 
-  if(WIN32)
-    # Set boost, openssl, zlib path
-    set(BOOST_INCLUDEDIR ${CppRest_ROOT_DIR}/boost/include)
-    set(BOOST_LIBRARYDIR ${CppRest_ROOT_DIR}/boost/boost_libs)
-  
-    set(OPENSSL_ROOT_DIR ${CppRest_ROOT_DIR}/openssl)
-    set(ZLIB_ROOT ${CppRest_ROOT_DIR}/zlib)
-  endif(WIN32)
 
 endif(CppRest_ROOT_DIR)
 
+message(${CppRest_LIBRARY})
+
+message(${CppRest_LIBRARY_DEBUG})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(CppRest DEFAULT_MSG CppRest_INCLUDE_DIR CppRest_LIBRARY)
 
